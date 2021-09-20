468fe53486b8832c6df9207d4528992886c069c2
Louis Bailleau <louis.bailleau@free.fr>
2021-09-20 21:47:05 +0200

    Bumped packages version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,121 +1,122 @@
 module.exports = {
-  title: 'PteroBilling',
-  tagline: 'An open-source Laravel 8 online store, client area, and billing software specially made for Pterodactyl panel',
-  url: 'https://docs.pterobilling.org',
-  baseUrl: '/',
-  favicon: 'img/favicon.webp',
-  organizationName: 'pterobilling',
-  projectName: 'docs',
-  plugins: ['@docusaurus/plugin-google-gtag'],
+  title: "PteroBilling",
+  tagline:
+    "An open-source Laravel 8 online store, client area, and billing software specially made for Pterodactyl panel",
+  url: "https://docs.pterobilling.org",
+  baseUrl: "/",
+  favicon: "img/favicon.webp",
+  organizationName: "pterobilling",
+  projectName: "docs",
+  plugins: ["@docusaurus/plugin-google-gtag"],
   themeConfig: {
     navbar: {
-      title: 'PteroBilling',
+      title: "PteroBilling",
       logo: {
-        alt: 'PteroBilling Icon',
-        src: 'img/icon.webp',
+        alt: "PteroBilling Icon",
+        src: "img/icon.webp",
       },
-      links: [
+      items: [
         {
-          href: 'https://pterobilling.org',
-          label: 'Website',
-          position: 'left',
+          href: "https://pterobilling.org",
+          label: "Website",
+          position: "left",
         },
         {
-          to: 'docs/welcome',
-          activeBasePath: 'docs',
-          label: 'Documentation',
-          position: 'left',
+          to: "docs/welcome",
+          activeBasePath: "docs",
+          label: "Documentation",
+          position: "left",
         },
         {
-          href: 'https://demo.pterobilling.org',
-          label: 'Demo',
-          position: 'left',
+          href: "https://demo.pterobilling.org",
+          label: "Demo",
+          position: "left",
         },
         //{to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/pterobilling/pterobilling',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/pterobilling/pterobilling",
+          label: "GitHub",
+          position: "right",
         },
         {
-          href: 'https://discord.gg/GQ5EWQz6MQ',
-          label: 'Discord',
-          position: 'right',
+          href: "https://discord.gg/GQ5EWQz6MQ",
+          label: "Discord",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Useful Links',
+          title: "Useful Links",
           items: [
             {
-              href: 'https://pterobilling.org',
-              label: 'Website',
+              href: "https://pterobilling.org",
+              label: "Website",
             },
             {
-              label: 'Documentation',
-              to: 'docs/welcome',
+              label: "Documentation",
+              to: "docs/welcome",
             },
           ],
         },
         {
-          title: 'Support',
+          title: "Support",
           items: [
             {
-              label: 'Discord',
-              href: 'https://discord.gg/GQ5EWQz6MQ',
+              label: "Discord",
+              href: "https://discord.gg/GQ5EWQz6MQ",
             },
             {
-              label: 'Github Discussions',
-              href: 'https://github.com/pterobilling/pterobilling/discussions',
+              label: "Github Discussions",
+              href: "https://github.com/pterobilling/pterobilling/discussions",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/pterobilling/pterobilling',
+              label: "GitHub",
+              href: "https://github.com/pterobilling/pterobilling",
             },
             {
-              label: 'Demo',
-              href: 'https://demo.pterobilling.org',
+              label: "Demo",
+              href: "https://demo.pterobilling.org",
             },
           ],
         },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Alaister Leung. All rights reserved.`,
     },
-    gtag: { trackingID: 'G-D37Y7G2S6Q' },
+    gtag: { trackingID: "G-D37Y7G2S6Q" },
   },
   scripts: [
     {
-      src: 'https://arc.io/widget.min.js#qtir15bF',
+      src: "https://arc.io/widget.min.js#qtir15bF",
       async: true,
     },
     {
-      src: 'https://static.cloudflareinsights.com/beacon.min.js',
-      'data-cf-beacon': '{\'token\': \'43934bd6f50a42f3bbc4b6a27ea97eec\'}',
+      src: "https://static.cloudflareinsights.com/beacon.min.js",
+      "data-cf-beacon": "{'token': '43934bd6f50a42f3bbc4b6a27ea97eec'}",
       defer: true,
     },
   ],
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/pterobilling/docs/edit/master/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/pterobilling/docs/edit/master/",
         },
         blog: {
           showReadingTime: true,
-          editUrl: 'https://github.com/pterobilling/docs/edit/master/blog/',
+          editUrl: "https://github.com/pterobilling/docs/edit/master/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
